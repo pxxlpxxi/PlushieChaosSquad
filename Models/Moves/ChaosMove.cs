2fc62ca33f3b6e1f481a3ce6eceeaae68b53d159
pxxlpxxi <vaikeflyger@hotmail.com>
2026-09-09 22:17:51 +0200

    Added model classes for Models/Moves-folder

--- Models/Moves/ChaosMove.cs
+++ Models/Moves/ChaosMove.cs
@@ -4,7 +4,35 @@ using System.Text;
 
 namespace PlushieChaosSquad.Models.Moves
 {
+    /// <summary>
+    /// Represents a basic chaos move that a plushie can perform.
+    /// </summary>
     internal class ChaosMove
     {
+        internal string Name { get; }
+        internal string Description { get; }
+        internal int EnergyCost { get; }
+        internal SkillSet SkillSet { get; }
+
+        /// <summary>
+        /// Creates a new basic chaos move.
+        /// </summary>
+        /// <param name="name">The name of the chaos move.</param>
+        /// <param name="description">A description of what happens when a plushie performs the move</param>
+        /// <param name="energyCost">The amount of ChaosEnergy required to perform the move.</param>
+        /// <param name="skillSet">The skill set associated with the move.</param>
+        internal ChaosMove(string name, string description, SkillSet skillSet, int energyCost = 10) { 
+            Name = name;
+            Description = description;
+            SkillSet = skillSet;
+            EnergyCost = energyCost;
+
+        }
+
+        /// <summary>
+        /// Executes the chaos move.
+        /// </summary>
+        /// <returns>A description of the chaos caused by the move.</returns>
+        internal string Execute() => Description;
     }
 }
--- Models/Moves/ChaoticFailureMove.cs
+++ Models/Moves/ChaoticFailureMove.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace PlushieChaosSquad.Models.Moves
 {
+    /// <summary>
+    /// Represents a chaotic reaction that occurs when attempts to perform 
+    /// a chaos move during a session that has already been used.
+    /// </summary>
     internal class ChaoticFailureMove
     {
+        internal string Name { get; }
+        internal string Description { get; }
+        internal int EnergyCost { get; }
+
+    
+        internal ChaoticFailureMove(string name, string description, int energyCost = 15) { 
+            Name = name;
+            Description = description;
+            EnergyCost = energyCost;
+        }
+
+        internal string Execute() => Description;
     }
 }
