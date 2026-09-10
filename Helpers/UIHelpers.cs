f0fdb77520499e05549b84b3850b6816b18cb456
pxxlpxxi <vaikeflyger@hotmail.com>
2026-09-10 23:45:31 +0200

    Added UIHelpers.cs and UIReport.cs

---
+++ Helpers/UIHelpers.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlushieChaosSquad.Helpers
+{
+    internal static class UIHelpers
+    {
+        internal static void WriteBlue(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine();
+            Console.WriteLine(text);
+            Console.WriteLine();
+
+            Console.ResetColor();
+        }
+
+        internal static void WriteGrey(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+
+            var cursorPosition = Console.GetCursorPosition();
+            int left = (Console.BufferWidth - text.Length) / 2;
+            Console.SetCursorPosition(left, cursorPosition.Top);
+
+            Console.Write(text);
+            Console.WriteLine();
+
+            var newPosition = Console.GetCursorPosition();
+            Console.SetCursorPosition(0, newPosition.Top);
+            Console.ResetColor();
+        }
+
+        internal static void WriteRed(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+
+            Console.WriteLine();
+
+            var cursorPosition = Console.GetCursorPosition();
+            int left = (Console.BufferWidth - text.Length) / 2;
+            Console.SetCursorPosition(left, cursorPosition.Top);
+
+            Console.WriteLine(text);
+            Console.WriteLine();
+
+            var newPosition = Console.GetCursorPosition();
+            Console.SetCursorPosition(0, newPosition.Top);
+
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Helpers/UIReport.cs
@@ -0,0 +1,67 @@
+﻿using PlushieChaosSquad.Models.Squad;
+
+namespace PlushieChaosSquad.Helpers
+{
+    internal static class UIReport
+    {
+        private static readonly Random random = new Random();
+        
+        internal static void WriteReport(
+            string title,
+            string report,
+            bool isResolved)
+        {
+            UIHelpers.WriteBlue(title);
+
+            Console.WriteLine();
+            Console.WriteLine(report);
+            Console.WriteLine($"Resolved: {isResolved}");
+            Console.WriteLine();
+        }
+        internal static void SOS() {
+
+            UIHelpers.WriteRed(sos[random.Next(sos.Length)]);
+        }
+
+        internal static void WriteObituary(Plushie plushie, string cause)
+        {
+            string[] obituary = [
+                $"In loving memory of rapidly declining health of {plushie.Name}",
+                $"whose Chaos Energy level reached a devastating {plushie.ChaosEnergy.ToString()}",
+                $"Cause of Exhaustion: {cause}",
+                $"{DateTime.Now}",
+                $"Observations related to the incident:\n"];
+
+            for (int i = 1; i<=obituary.Length; i++)
+            {
+                UIHelpers.WriteGrey(obituary[i-1]);
+            }
+        }
+
+        internal static string[] sos = [
+            "Oh no!",
+            "H E L P !",
+            "mAyDaY! MaYdAY!",
+            "Oh no no no nononononononoNONnOnoONONO !",
+            "oh GOD pLEASE nO",
+            "We have an emergency!",
+            "Make it stop!"
+            ];
+    }
+}
+
+
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace PlushieChaosSquad.Helpers
+//{
+//    internal static class UIReport
+//    {
+//        internal static void WriteReport(string report) 
+//        {
+//            Console.WriteLine(report);
+//        }
+//    }
+//}
