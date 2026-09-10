0fc1e5bcaff4c5ce3459e06038dcae35cd6a658e
pxxlpxxi <vaikeflyger@hotmail.com>
2026-09-10 14:03:54 +0200

    Updated IDispatchStrategy.cs

--- Interfaces/IDispatchStrategy.cs
+++ Interfaces/IDispatchStrategy.cs
@@ -1,10 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using PlushieChaosSquad.Models.Incidents;
+using PlushieChaosSquad.Models.Squad;
 
 namespace PlushieChaosSquad.Interfaces
 {
+    /// <summary>
+    /// Defines a strategy for selecting a plushie to handle a chaos incident.
+    /// </summary>
     internal interface IDispatchStrategy
     {
+        /// <summary>
+        /// Selects a strategy for selecting a plushie to handle a chaos incident.
+        /// </summary>
+        /// <param name="incident">The chaos incident that needs to be handled.</param>
+        /// <param name="plushies">The plushies available for selection.</param>
+        /// <returns></returns>
+        internal Plushie SelectPlushie(ChaosIncident incident, List<Plushie> plushies);
+ 
     }
 }
