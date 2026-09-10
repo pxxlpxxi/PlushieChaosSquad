ef67f2edf1c5551b9acd035606566978ee6b00ae
pxxlpxxi <vaikeflyger@hotmail.com>
2026-09-10 14:04:34 +0200

    Updated FirstAvailableStrategy.cs

--- Strategies/FirstAvailableStrategy.cs
+++ Strategies/FirstAvailableStrategy.cs
@@ -1,10 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using PlushieChaosSquad.Exceptions;
+using PlushieChaosSquad.Helpers;
+using PlushieChaosSquad.Interfaces;
+using PlushieChaosSquad.Models.Incidents;
+using PlushieChaosSquad.Models.Squad;
 
 namespace PlushieChaosSquad.Strategies
 {
-    internal class FirstAvailableStrategy
+    /// <summary>
+    /// Selects the first available plushie from the provided list.
+    /// </summary>
+    internal class FirstAvailableStrategy : IDispatchStrategy
     {
+        /// <summary>
+        /// Selects the first available plushie to handle the chaos incident.
+        /// </summary>
+        /// <returns></returns>
+         Plushie IDispatchStrategy.SelectPlushie(ChaosIncident incident, List<Plushie> plushies) {
+            if (incident == null) throw new NoSuitablePlushieException();
+
+            Plushie? plushie = SearchHelper.FindFirst(
+                plushies,
+                plushie => plushie.IsAvailable);
+
+            if (plushies == null) throw new NoSuitablePlushieException();
+
+            return plushie;
+        }
+
     }
 }
