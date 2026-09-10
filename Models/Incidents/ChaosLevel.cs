9c55a6c6a2e9aa671ae85b368fa86a67e879f2ae
pxxlpxxi <vaikeflyger@hotmail.com>
2026-09-10 14:01:22 +0200

    Updated ChaosLevel and enum values

--- Models/Incidents/ChaosLevel.cs
+++ Models/Incidents/ChaosLevel.cs
@@ -1,16 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace PlushieChaosSquad.Models.Incidents
+﻿namespace PlushieChaosSquad.Models.Incidents
 {
     /// <summary>
     /// Represents the severity level of a chaos incident.
     /// </summary>
     internal enum ChaosLevel
     {
-        Low,
-        Medium,
-        High
+        /// <summary>
+        /// Corresponds to the Chaos Energy required for one basic chaos move.
+        /// </summary>
+        Low = 10,
+
+        /// <summary>
+        /// Corresponds to the Chaos Energy required for two basic chaos moves.
+        /// </summary>
+        Medium = 20,
+
+        /// <summary>
+        /// Corresponds to the Chaos Energy required for one signature chaos one basic chaos move.
+        /// </summary>
+        High = 30,
     }
 }
