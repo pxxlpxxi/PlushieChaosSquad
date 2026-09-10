fc84dd5b60f2af77b1842397f10adb77436d9559
pxxlpxxi <vaikeflyger@hotmail.com>
2026-09-10 15:56:32 +0200

    Expanded ChaosMove.cs and ChaosMoveLibrary.cs with Intent, Success, Failure properties

--- Models/Moves/ChaosMove.cs
+++ Models/Moves/ChaosMove.cs
@@ -10,7 +10,9 @@ namespace PlushieChaosSquad.Models.Moves
     internal class ChaosMove
     {
         internal string Name { get; }
-        internal string Description { get; }
+        internal string Intent { get; }
+        internal string Success { get; }
+        internal string Failure { get; }
         internal int EnergyCost { get; }
         internal SkillSet SkillSet { get; }
 
@@ -18,21 +20,26 @@ namespace PlushieChaosSquad.Models.Moves
         /// Creates a new basic chaos move.
         /// </summary>
         /// <param name="name">The name of the chaos move.</param>
-        /// <param name="description">A description of what happens when a plushie performs the move</param>
+        /// <param name="name">The name of the chaos move.</param>
+        /// <param name="intent">A description of what the plushie intends or is supposed to do.</param>
+        /// <param name="success">A description of what happens when the move succeeds.</param>
+        /// <param name="failure">A description of what happens when the move fails because the intended chaos has already occurred.</param>
         /// <param name="energyCost">The amount of ChaosEnergy required to perform the move.</param>
         /// <param name="skillSet">The skill set associated with the move.</param>
-        internal ChaosMove(string name, string description, SkillSet skillSet, int energyCost = 10) { 
+        internal ChaosMove(string name, string intent, string success, string failure, SkillSet skillSet, int energyCost = 10) { 
             Name = name;
-            Description = description;
+            Intent = intent;
+            Success = success;
+            Failure=failure;
             SkillSet = skillSet;
             EnergyCost = energyCost;
 
         }
 
         /// <summary>
-        /// Executes the chaos move.
+        /// Executes the chaos move as a standalone action.
         /// </summary>
         /// <returns>A description of the chaos caused by the move.</returns>
-        internal string Execute() => Description;
+        internal string Execute() => Success;
     }
 }
